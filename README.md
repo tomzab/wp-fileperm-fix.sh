1d5b1339fc05d996f3934ca193ff201f92d86b60
Tomek <tomek@imperodesign.com>
2016-04-14 12:44:47 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # wp-fileperm-fix.sh
 Wordpress file permissions fix shell script
+
+To use it - copy the file to your WP root folder, add an executable flag:
+
+`chmod +x wp-fileperm-fix.sh`
+
+Theh execute as root with:
+
+`sudo ./wp-fileperm-fix.sh`
---
+++ wp-fileperm-fix.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# This script configures WordPress file permissions based on recommendations
+# from http://codex.wordpress.org/Hardening_WordPress#File_permissions
+#
+# Author: Michael Conigliaro
+#
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+WP_OWNER=beefeater # &lt;-- wordpress owner
+WP_GROUP=beefeater # &lt;-- wordpress group
+WP_ROOT=. # &lt;-- wordpress root directory
+WS_GROUP=www-data # &lt;-- webserver group
+
+# reset to safe defaults
+find ${WP_ROOT} -exec chown ${WP_OWNER}:${WP_GROUP} {} \;
+find ${WP_ROOT} -type d -exec chmod 755 {} \;
+find ${WP_ROOT} -type f -exec chmod 644 {} \;
+
+# allow wordpress to manage wp-config.php (but prevent world access)
+chgrp ${WS_GROUP} ${WP_ROOT}/wp-config.php
+chmod 660 ${WP_ROOT}/wp-config.php
+
+# allow wordpress to manage .htaccess
+touch ${WP_ROOT}/.htaccess
+chgrp ${WS_GROUP} ${WP_ROOT}/.htaccess
+chmod 664 ${WP_ROOT}/.htaccess
+
+# allow wordpress to manage wp-content
+find ${WP_ROOT}/wp-content -exec chgrp ${WS_GROUP} {} \;
+find ${WP_ROOT}/wp-content -type d -exec chmod 775 {} \;
+find ${WP_ROOT}/wp-content -type f -exec chmod 664 {} \;
